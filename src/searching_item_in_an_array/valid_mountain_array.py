2c141eefe1983ef7afe559209da7cea36bd92572
alessandro.maccario <alessandro.maccario@proton.me>
2026-01-15 21:41:22 +0100

    feat: add scrpt for the mountain array

--- src/searching_item_in_an_array/valid_mountain_array.py
+++ src/searching_item_in_an_array/valid_mountain_array.py
@@ -7,6 +7,12 @@ Reference:
 
 from typing import List
 
+# arr = [0, 3, 2, 1]
+# arr = [2, 1]
+# arr = [3, 5, 5]
+# arr = [4, 4, 3, 2, 1]
+arr = [1, 2, 3, 4, 9, 8, 7, 6, 5]
+
 
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
@@ -32,12 +38,12 @@ class Solution:
 
         Example 1
         ---------
-        Input: arr = [2,1]
+        Input: arr = [2,1] -> len(arr) < 3
         Output: false
 
         Example 2
         ---------
-        Input: arr = [3,5,5]
+        Input: arr = [3,5,5] -> increasing, but not strictly
         Output: false
 
         Example 3
@@ -46,10 +52,63 @@ class Solution:
         Output: true
 
         """
+        i = 0
+
         if len(arr) < 3:
+            print("Too short")
             return False
         else:
             # add conditions
-            pass
+            len_array = len(
+                arr
+            )  # needed to find where the center of the arr can be found
+            middle_index = len_array // 2
+            print("Middle index:", middle_index)
+
+            if len(arr) % 2 == 0:
+                while i < len(arr[: middle_index - 1]):
+                    print(
+                        f"i is: {i} | middle index: {middle_index} | first part of the array {arr[:middle_index]} | arr+1 = {arr[i + 1]}"
+                    )
+                    if arr[i] >= arr[i + 1]:
+                        # the first part of the array is in decreasing order
+                        print("First halves, False")
+                        return False
+                    else:
+                        print("First part of the array increasing order ↗️")
+                    i += 1
+                    print(f"Current i: {i}")
+                print(f"Current arr at position i = {i} -> {arr[i]}")
+                while i < len(arr[middle_index:]):
+                    if arr[i] <= arr[i + 1]:
+                        print("Second halves, False")
+                        return False
+                    else:
+                        print("Second part of the array in decreasing order ↘️")
+                        return True
+            else:
+                while i < len(arr[:middle_index]):
+                    print(
+                        f"i is: {i} | middle index: {middle_index} | first part of the array {arr[:middle_index]} | arr+1 = {arr[i + 1]}"
+                    )
+                    if arr[i] >= arr[i + 1]:
+                        # the first part of the array is in decreasing order
+                        print("First halves, False")
+                        return False
+                    else:
+                        print("First part of the array increasing order ↗️")
+                    i += 1
+                    print(f"Current i: {i}")
+                print(f"Current arr at position i = {i} -> {arr[i]}")
+                while i < len(arr[middle_index:]):
+                    if arr[i] <= arr[i + 1]:
+                        print("Second halves, False")
+                        return False
+                    else:
+                        print("Second part of the array in decreasing order ↘️")
+                        return True
 
         return False
+
+
+print(Solution().validMountainArray(arr=arr))
