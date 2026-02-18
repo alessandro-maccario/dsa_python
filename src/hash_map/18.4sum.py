78da8a91b968b80fb616b848e6e1d65c407d26c5
alessandro.maccario <alessandro.maccario@proton.me>
2026-02-18 19:39:04 +0100

    initial commit: 4sum script challenge

---
+++ src/hash_map/18.4sum.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        pass
+
+
+#################
+### TEST CASE ###
+#################
+
+nums = [1, 0, -1, 0, -2, 2]
+target = 0
+solution = Solution()
+print(solution.fourSum(nums=nums, target=target))
