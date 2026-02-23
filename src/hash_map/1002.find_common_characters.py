e8928bdb6abc3cdb96a97f0af3c66d37cc90345b
alessandro.maccario <alessandro.maccario@proton.me>
2026-02-23 20:24:00 +0100

    feat: finish find common characters script challenge

--- src/hash_map/1002.find_common_characters.py
+++ src/hash_map/1002.find_common_characters.py
@@ -3,6 +3,31 @@ from typing import List
 
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
+        """
+        Given a string array words, return an array of all characters that show up
+        in all strings within the words (including duplicates).
+        You may return the answer in any order.
+
+        Parameters
+        ----------
+        words : List[str]
+            A list of strings.
+
+        Returns
+        -------
+        List[str]
+            Return an array of all characters that show up in all strings within the words (including duplicates).
+
+        Examples
+        --------
+        Example 1:
+        Input: words = ["bella","label","roller"]
+        Output: ["e","l","l"]
+
+        Example 2:
+        Input: words = ["cool","lock","cook"]
+        Output: ["c","o"]
+        """
         temp_list = []
 
         for word in words:
@@ -17,8 +42,9 @@ class Solution:
                     freq_map[char] += 1
             temp_list.append(freq_map)
 
-        # for each char of the first word, look at all of the rest of the sub dictionaries, and find the minumum
-        # amount of occurrences for each char that is available in all of the sub dictionaries
+        # for each char of the first word, look at all of the rest of the sub dictionaries, and find the minimum
+        # amount of occurrences for each char that is available in all of the sub dictionaries. The first
+        # word must contain already all the characters available in all of the other strings.
         result = []
         temp_val = []
 
@@ -26,7 +52,7 @@ class Solution:
             for sub_dict in temp_list:
                 temp_val.append(sub_dict.get(char, 0))
 
-            # if True, it means that the char is not appearing in all of the words
+            # if 0 in the list, it means that the char is not appearing in all of the words; otherwise it does
             if 0 not in temp_val:
                 result.extend([char] * min(temp_val))
 
