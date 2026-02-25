b42949854f5125745b0d0ae96e824c3bf32f8c7c
alessandro.maccario <alessandro.maccario@proton.me>
2026-02-25 17:51:40 +0100

    feat: finish contiguous array script challenge

--- src/hash_map/525.contiguous_array.py
+++ src/hash_map/525.contiguous_array.py
@@ -3,22 +3,37 @@ from typing import List
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        """_summary_
+        """
+        Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
 
         Parameters
         ----------
         nums : List[int]
-            _description_
+            A binary array nums
 
         Returns
         -------
         int
-            _description_
+            Return the maximum length of a contiguous subarray with an equal number of 0 and 1.
 
         Examples
         --------
-        """
+        Example 1:
+        Input: nums = [0,1]
+        Output: 2
+        Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
 
+        Example 2:
+        Input: nums = [0,1,0]
+        Output: 2
+        Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+        Example 3:
+        Input: nums = [0,1,1,1,1,1,0,0,0]
+        Output: 6
+        Explanation: [1,1,1,0,0,0] is the longest contiguous subarray with equal number of 0 and 1.
+        """
+        # initialize dict: useful if the longest contiguous subarray starts with the very first element
         dict_idx_sum_prefix = {0: -1}
         maximum_length_so_far = 0
         current_sum = 0
