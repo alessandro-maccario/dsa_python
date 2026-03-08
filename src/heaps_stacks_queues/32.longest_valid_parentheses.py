8ebae30f69cac46c58cfa02e071f27b18a100dad
alessandro.maccario <alessandro.maccario@proton.me>
2026-03-08 11:27:10 +0100

    initial commit: longest valid parenthesis

---
+++ src/heaps_stacks_queues/32.longest_valid_parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        pass
+
+
+#################
+### TEST CASE ###
+#################
+
+s = "(()"
+
+solution = Solution()
+print(solution.longestValidParentheses(s=s))
