f6a630aafd0ba08491483f30ed5c8031158a920b
alessandro.maccario <alessandro.maccario@proton.me>
2026-04-07 18:14:47 +0200

    feat: add append method for linked list

--- src/linked_lists/dsa_udemy_singly_linked_list.py
+++ src/linked_lists/dsa_udemy_singly_linked_list.py
@@ -5,58 +5,60 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self, value):
-        # The constructor creates a new node
-        new_node = Node(value)
-        # attach the head of the linked list to the current new node
-        self.head = new_node
-        # attach the tail of the linked list to the current new node
-        self.tail = new_node
-        # we are gonna keep track of the length
-        self.length = 1
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def append(self, value):
+        """
+        Append a node to the end of a Linked List.
+
+        Parameters
+        ----------
+        value : int
+            Integer to append to the Linked List.
+        """
+        # 1. Create a new node
+        new_node = Node(value=value)
+
+        # in case the Linked List is empty just point to the new node
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            # 2. Point the tail.next and tail to the new node
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def __repr__(self) -> str:
+        """
+        Create custom representation of the Linked List.
+
+        Returns
+        -------
+        str
+            The string representation of the Linked List
+        """
+        node = self.head
+        nodes = []
+
+        while node is not None:
+            nodes.append(str(node.value))
+            node = node.next
+        nodes.append("None")
+        return " -> ".join(nodes)
 
 
-# Create nodes
-node1 = Node(11)
-node2 = Node(3)
-node3 = Node(23)
-node4 = Node(7)
-# Link nodes
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = None
+# Create Linked List and Nodes
+linked_list = LinkedList()
 
-# Head points to the first node and tail to the last one
-head = node1
-tail = node4
-
-
-print(tail.next)
-
-
-def print_list(head):
-    temp = head
-    while (
-        temp is not None
-    ):  # until we haven't reach the end of the list, we print the value
-        print(temp.value)
-        temp = temp.next
-
-
-#########################
-### Append a new node ###
-#########################
-
-# 1. Create a new node
-new_node = Node(5)
-
-# 2. Point the last node to the new node
-node4.next = new_node
-
-# 3. Point the tail to the new node
-tail.next = new_node
-
+# Append a new node
+linked_list.append(value=25)
+linked_list.append(value=40)
+linked_list.append(value=50)
 
 # 4. Check if the new node has been added to the linked list
-print_list(head=head)
+print(linked_list)
